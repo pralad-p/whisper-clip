878370d84adb17bcd6dd433e93ddbcbd0872b1af
Pralad Prasad <pralad.prasad@gmail.com>
2024-12-21 00:22:59 +0100

    personal changes

--- whisper_client.py
+++ whisper_client.py
@@ -1,9 +1,12 @@
 import whisper
+import torch
 
 
 class WhisperClient:
     def __init__(self, model_name="medium.en"):
-        self.model = whisper.load_model(model_name)
+        device = None
+        self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = whisper.load_model(model_name,device=self.device)
 
     def transcribe(self, audio_path):
         result = self.model.transcribe(audio_path)
